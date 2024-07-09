f7b35055285202a3ef1d0bb1b7783dcafa557993
Harish <harish.16634@gmail.com>
2024-07-09 21:10:44 +0530

    added photo feature

--- app/dashboard/(components)/your-spaces.tsx
+++ app/dashboard/(components)/your-spaces.tsx
@@ -6,6 +6,7 @@ import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import RenderStats from './render-stats';
+import Image from 'next/image';
 
 export default async function YourSpaces() {
   const user = await currentUser();
@@ -18,6 +19,7 @@ export default async function YourSpaces() {
     .select({
       id: spaceTable.id,
       url: spaceTable.spaceUrl,
+      imageUrl: spaceTable.imageUrl,
     })
     .from(spaceTable)
     .where(eq(spaceTable.userId, user.id));
@@ -29,7 +31,16 @@ export default async function YourSpaces() {
           <div className="bg-gray-800 flex px-10 py-4 border rounded-lg border-slate-400 w-80">
             <div className="flex items-center justify-between gap-8">
               <div>
-                <Images />
+                {item.imageUrl ? (
+                  <Image
+                    src={item.imageUrl!}
+                    height={40}
+                    width={60}
+                    alt="Image"
+                  />
+                ) : (
+                  <Images />
+                )}
               </div>
 
               <div className="text-xl text-semibold flex flex-col gap-2">
